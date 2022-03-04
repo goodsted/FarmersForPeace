d23b4991675efb0faf2f859bb78831f2acb3ee95
Yunus Gülcü <yunus.gulcu35@gmail.com>
2022-03-04 15:01:37 +0300

    Revert "Add sentry for error logging."
    This reverts commit f1d7ab70f12c352344de28f1bd0196f0608e609f.

--- pages/_document.js
+++ pages/_document.js
@@ -8,9 +8,9 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Pragati+Narrow:wght@400;700&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Pragati+Narrow:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
