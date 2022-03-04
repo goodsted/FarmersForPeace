c3669ee9b366a600cd0b063b63d7bc4cb34ac521
Yunus Gülcü <yunus.gulcu35@gmail.com>
2022-03-04 15:33:12 +0300

    add google analytics :(

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Footer from '../components/Footer';
 import { Grid, Typography, Button } from '@mui/material';
 import Link from 'next/link';
 import { useMediaQuery } from '@mui/material';
+import Script from 'next/script';
 
 export default function Home() {
   const smMatch = useMediaQuery('(max-width:900px)');
@@ -26,6 +27,18 @@ export default function Home() {
   };
   return (
     <div>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=UA-222104013-1"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-222104013-1');
+        `}
+      </Script>
       <Head>
         <title>Action for Ukraine | Farmers for Peace</title>
         <meta
@@ -59,25 +72,25 @@ export default function Home() {
               ></iframe>
             </Grid>
             <Grid item xs={12} md={5.4} sx={rightColumn}>
-                <Grid container sx={{ pb: 2.5,display: {xs:'none',md:'initial'} }}>
-                  <Typography variant="h1" color="primary" sx={{ mb: 2 }}>
-                    Action for Ukraine
-                  </Typography>
-                  <Typography variant="body1" sx={{ mb: 2 }}>
-                    A lot of people may need to leave Ukraine in the near future and all of them need a place to stay, an activity to do and most of all loving human connections.
-                  </Typography>
-                  <Typography variant="body1" sx={{ mb: 2 }}>
-                    At the same time, a lot of European farmers suffer from loneliness due to their often remote locations and they also often have trouble to find helping hands, especially in the
-                    upcoming spring season.
-                  </Typography>
-                  <Typography variant="body1" sx={{ mb: 2 }}>
-                    If there are farmers which are willing to host refugees from Ukraine, submit your interest and you&apos;ll appear on the map for those looking to find a safe space.
-                  </Typography>
-                  <Typography variant="body1" sx={{ mb: 2 }}>
-          If you are not a farmer but still would like to host refugees, you can also take part!
-          </Typography>
-                </Grid>
-              
+              <Grid container sx={{ pb: 2.5, display: { xs: 'none', md: 'initial' } }}>
+                <Typography variant="h1" color="primary" sx={{ mb: 2 }}>
+                  Action for Ukraine
+                </Typography>
+                <Typography variant="body1" sx={{ mb: 2 }}>
+                  A lot of people may need to leave Ukraine in the near future and all of them need a place to stay, an activity to do and most of all loving human connections.
+                </Typography>
+                <Typography variant="body1" sx={{ mb: 2 }}>
+                  At the same time, a lot of European farmers suffer from loneliness due to their often remote locations and they also often have trouble to find helping hands, especially in the
+                  upcoming spring season.
+                </Typography>
+                <Typography variant="body1" sx={{ mb: 2 }}>
+                  If there are farmers which are willing to host refugees from Ukraine, submit your interest and you&apos;ll appear on the map for those looking to find a safe space.
+                </Typography>
+                <Typography variant="body1" sx={{ mb: 2 }}>
+                  If you are not a farmer but still would like to host refugees, you can also take part!
+                </Typography>
+              </Grid>
+
 
               <Grid
                 container
@@ -87,8 +100,8 @@ export default function Home() {
                 sx={{
                   pb: { xs: 2, md: 4 },
                   pt: { xs: 2, md: 2 },
-                  pl:{xs: 2,md:0},
-                  pr:{xs: 2,md:0},
+                  pl: { xs: 2, md: 0 },
+                  pr: { xs: 2, md: 0 },
                   borderTop: 1,
                   borderColor: 'grey.middle',
                   background: '#fff',
@@ -97,19 +110,19 @@ export default function Home() {
                     md: 'sticky',
                   },
                   bottom: 0,
-                  mt:{xs: 'auto'}
+                  mt: { xs: 'auto' }
                 }}
               >
                 <Grid item xs={6}>
                   <Link href="/host" passHref>
-                    <Button fullWidth variant="contained" color="primary" sx={{ p: {xs:1,md:2} ,fontSize: {xs:16,md: 'initial'} }}>
+                    <Button fullWidth variant="contained" color="primary" sx={{ p: { xs: 1, md: 2 }, fontSize: { xs: 16, md: 'initial' } }}>
                       I want to host
                     </Button>
                   </Link>
                 </Grid>
                 <Grid item xs={6}>
                   <Link href="/need-help" passHref>
-                    <Button fullWidth variant="outlined" color="primary" sx={{ p: {xs:1,md:2} ,fontSize: {xs:16,md: 'initial'} }}>
+                    <Button fullWidth variant="outlined" color="primary" sx={{ p: { xs: 1, md: 2 }, fontSize: { xs: 16, md: 'initial' } }}>
                       I need help
                     </Button>
                   </Link>
